28565e41906fb5336dba930374ac3b843370799c
GabMic <ruinsain@gmail.com>
2017-12-08 15:29:20 +0200

    Add tabs and tab components

---
+++ Tabs.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <div class="tabs is-centered is-large is-boxed">
+            <ul>
+                <li v-for="tab in tabs" :class="{ 'is-active': tab.isActive }">
+                    <a @click="selectTab(tab)">{{ tab.name }}</a>
+                </li>
+            </ul>
+        </div>
+        <div class="tabs-details">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+
+<script>
+    import Tab from '../components/Tab.vue';
+    export default{
+        components: {Tab},
+        data() {
+            return { tabs: [] };
+        },
+        created() {
+            this.tabs = this.$children;
+        },
+        methods: {
+            selectTab(selectedTab) {
+                this.tabs.forEach(tab => {
+                    tab.isActive = (tab.name == selectedTab.name);
+                });
+            }
+        }
+    }
+</script>
